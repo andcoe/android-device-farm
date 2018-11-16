ced4e3a7e8a6ac48c1f361d5a71091c0ec492150
chfe <chris.fey@zuhlke.com>
2018-11-16 14:19:33 +0000

    helloworld python adb

--- setupDevices.py
+++ setupDevices.py
@@ -8,13 +8,19 @@ from adb import adb_commands
 from adb import sign_m2crypto
 
 
+
 # KitKat+ devices require authentication
 signer = sign_m2crypto.M2CryptoSigner(
     op.expanduser('~/.android/adbkey'))
 # Connect to the device
+
 device = adb_commands.AdbCommands()
-print(device)
-device.ConnectDevice(
-    rsa_keys=[signer])
-# Now we can use Shell, Pull, Push, etc!
-print(device.Shell('echo asdf'))
+
+devices = device.Devices()
+for device in devices :
+    print(device)
+    print(device.Shell('echo asdf'))
+# device.ConnectDevice(
+#     rsa_keys=[signer])
+# # Now we can use Shell, Pull, Push, etc!
+# print(device.Shell('echo asdf'))
