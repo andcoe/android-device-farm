e69c8773677a67e83c61c4234c56145ea5fc5b9a
Andre <andre.marques.coelho@gmail.com>
2018-11-16 10:10:04 +0000

    rotate 3 ports

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("6555");
-});
+var port = 6555;
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    response.end("" + port);
+    if (port === 6557) port = 6555;
+    else port++;
+  })
+  .listen(8000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
