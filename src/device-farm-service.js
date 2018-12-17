ae93bb1a685f879e975e1c7d78a9096b08c8dc70
Andre <andre.marques.coelho@gmail.com>
2018-12-17 17:22:01 +0000

    feat: add postman collection

--- src/device-farm-service.js
+++ src/device-farm-service.js
@@ -1,5 +1,6 @@
-const moment = require("moment");
+const moment = require('moment');
 const _ = require('lodash');
+const Lease = require('./model/Lease');
 
 class DeviceFarmService {
 
@@ -21,9 +22,8 @@ class DeviceFarmService {
         const device = this.adbMonitor.preparedDevices.find(device => device.id === id);
         //never more than 1 matching lease
         //no stale leases
-        const lease = this.leases.find(lease => lease.deviceId === device.id);
+        const lease = this.leases.find(lease => lease.device.id === device.id);
 
-        console.log(lease);
         return {
             ...device,
             lease: !!lease ? lease : null
@@ -36,7 +36,7 @@ class DeviceFarmService {
 
     leaseAny() {
         const device = this.adbMonitor.preparedDevices
-            .find(device => _.every(this.leases, lease => lease.deviceId !== device.id));
+            .find(device => _.every(this.leases, lease => lease.device.id !== device.id));
 
         if (device) {
             const lease = new Lease(device, moment().add(10, 'minutes'));
@@ -47,11 +47,9 @@ class DeviceFarmService {
     }
 
     release(leaseId) {
-        console.log('leaseId', leaseId);
-        const removed = _.remove(this.leases, l => l.id === leaseId);
-        console.log(removed);
-        if(_.isEmpty(removed)) {
-           throw new LeaseNotFound(leaseId);
+        const removed = _.remove(this.leases, lease => lease.id === leaseId);
+        if (_.isEmpty(removed)) {
+            throw new LeaseNotFound(leaseId);
         }
     }
 }
@@ -73,14 +71,6 @@ class NoDevicesAvailable extends DeviceFarmError {
 class LeaseNotFound extends DeviceFarmError {
     constructor(leaseId) {
         super(`Lease not found with id:${leaseId}.`, 'LeaseNotFound');
-    }
-}
-
-class Lease {
-    constructor(device, inUseUntil) {
-        this.id = Math.floor(Math.random() * 1000000000).toString(); //TODO: improve this with unique ids
-        this.device = device;
-        this.inUseUntil = inUseUntil;
     }
 }
 
