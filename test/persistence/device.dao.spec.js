4e47dd3ac189745e48017440f9947d77b4f9c541
Andre <andre.marques.coelho@gmail.com>
2018-12-18 10:47:38 +0000

    test: devicedao create and get

--- test/persistence/device.dao.spec.js
+++ test/persistence/device.dao.spec.js
@@ -2,15 +2,16 @@ const DeviceDao = require('../../src/persistence/device.dao');
 const Device = require('../../src/model/device');
 
 describe('DeviceDao', () => {
-    describe('devices()', () => {
-        it('returns empty devices', () => {
-            const deviceDao = new DeviceDao();
-            expect(deviceDao.devices()).toEqual([]);
-        });
-        it('returns devices', () => {
-            const deviceDao = new DeviceDao();
-            deviceDao.create(new Device('123'));
-            expect(deviceDao.devices()).toEqual([new Device('123')]);
-        });
-    })
+
+    it('returns empty devices', () => {
+        const deviceDao = new DeviceDao();
+        expect(deviceDao.devices()).toEqual([]);
+    });
+    it('creates and returns devices', () => {
+        const deviceDao = new DeviceDao();
+        deviceDao.create(new Device('123'));
+        deviceDao.create(new Device('456'));
+        expect(deviceDao.devices()).toEqual([new Device('123'), new Device('456')]);
+    });
+
 });
