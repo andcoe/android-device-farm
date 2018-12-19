19790f9d83c270f6937f5e164a263081ca171c04
Andre Coelho <andre.marques.coelho@gmail.com>
2018-12-19 23:15:19 +0000

    refactor: inline methods

--- src/main/kotlin/org/andcoe/adf/devices/DeviceService.kt
+++ src/main/kotlin/org/andcoe/adf/devices/DeviceService.kt
@@ -4,17 +4,11 @@ import org.andcoe.adf.core.AdbDevice
 
 class DeviceService(private val deviceDao: DeviceDao) {
 
-    fun devices(): Map<DeviceId, Device> {
-        return deviceDao.devices()
-    }
+    fun devices(): Map<DeviceId, Device> = deviceDao.devices()
 
-    fun devices(deviceId: DeviceId): Device? {
-        return deviceDao.devices(deviceId)
-    }
+    fun devices(deviceId: DeviceId): Device? = deviceDao.devices(deviceId)
 
-    fun create(adbDevice: AdbDevice): Device {
-        return deviceDao.create(adbDevice)
-    }
+    fun create(adbDevice: AdbDevice): Device = deviceDao.create(adbDevice)
 
     fun remove(deviceId: DeviceId) {
         deviceDao.remove(deviceId)
