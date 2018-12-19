507fe8ab4b18bda1d42ecb5dcfeadc3f1e8ef0e7
Andre <andre.marques.coelho@gmail.com>
2018-12-19 15:19:41 +0000

    test: device id not found ex

--- src/test/kotlin/org/andcoe/adf/devices/DeviceResourceTest.kt
+++ src/test/kotlin/org/andcoe/adf/devices/DeviceResourceTest.kt
@@ -1,6 +1,8 @@
 package org.andcoe.adf.devices
 
+import org.andcoe.adf.exceptions.ResourceNotFound
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
 import util.DeviceResourceUtils.Companion.resourceReturningDevices
 import util.DeviceResourceUtils.Companion.resourceReturningEmptyDevices
@@ -28,5 +30,11 @@ class DeviceResourceTest {
     fun returnsDeviceById() {
         val deviceResource = resourceReturningDevices()
         assertThat(deviceResource.devices("PIXEL")).isEqualTo(Device(DeviceId("PIXEL")))
+    }
+
+    @Test
+    fun throwsIfNoDeviceFoundById() {
+        val deviceResource = resourceReturningDevices()
+        assertThatThrownBy { deviceResource.devices("random-id") }.isInstanceOf(ResourceNotFound::class.java)
     }
 }
